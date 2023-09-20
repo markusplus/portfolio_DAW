64a6e3668281ee28af5b1f88b6f957330c9f184a
markusplus <73444818+markusplus@users.noreply.github.com>
2023-09-20 14:32:26 +0000

    Ejercicios 4 y 5 DWS

---
+++ DWS/UD2/Ejerc5_For.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>Suma 10 primeros</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="Tablas de multiplicar" content="Geany 1.35" />
+</head>
+
+<body>
+	<?php
+        $result = "La suma de los 10 primeros números es: ";
+        $nums = 0;
+        for ($i=1; $i <= 10; $i++) { 
+            $nums += $i;
+            if ($i == 1) {
+                $result = $result . " $i ";
+            }
+            else if($i > 1 && $i < 10) {
+                $result = $result . " +$i ";
+            }
+            else if($i == 10) {
+                $result = $result . " = " . $nums;
+            }
+        }
+        echo $result;
+	?>
+</body>
+
+</html>
