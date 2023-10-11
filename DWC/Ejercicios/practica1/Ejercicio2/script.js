e6d0e8412ab05323a087328cbedffb004508b6ca
mjovcab <marcjovani16@gmail.com>
2023-10-11 17:31:56 +0000

    DWC

---
+++ DWC/Ejercicios/practica1/Ejercicio2/script.js
@@ -0,0 +1,20 @@
+function calculaPromedios(mapa) {
+    i = 0
+    let array = Array.from(mapa.keys())
+    result = array.map(function(obj) {
+       obj = [obj[0], ]
+    });
+    return result
+}
+
+function promedio(array) {
+    array.foreach((number) => )
+}
+
+const calificaciones = new Map([
+    ["Marc", [85, 90, 80]],
+    ["David", [60, 85, 60]],
+    ["Juan", [40, 50, 70]],
+]);
+
+alert(calculaPromedios(calificaciones))
