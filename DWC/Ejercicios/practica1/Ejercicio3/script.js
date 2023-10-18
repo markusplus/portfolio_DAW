5610bdf524d34b8e2acc26300c1d6fe512bc1380
mjovcab <marcjovani16@gmail.com>
2023-10-18 14:06:49 +0000

    DWS-Ejercicio3

--- DWC/Ejercicios/practica1/Ejercicio3/script.js
+++ DWC/Ejercicios/practica1/Ejercicio3/script.js
@@ -6,9 +6,18 @@ function generarVariableAleatoria(cantidad) {
         var u1 = Math.random()
         var u2 = Math.random()
         var z = Math.sqrt(-2 * Math.log(u1)) * Math.cos(2 * Math.PI * u2)
-        numeros.push(numeroNormal)
+        numeros.push(z)
     }
     return numeros
 }
 
+function generaDistribucionNormal(array) {
+    const media = 0
+    const varianza = 1
+    const d = array.map((d) => (d - media) / varianza)
+    return d
+}
+
 var randomVariable = generarVariableAleatoria(1000)
+document.getElementById("texto").innerHTML = randomVariable
+alert(d)
