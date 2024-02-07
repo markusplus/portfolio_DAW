be1159394d809dec1b460ee98c8c959a2759a2c1
markusplus <73444818+markusplus@users.noreply.github.com>
2024-02-07 22:31:12 +0000

    portfolio

--- DWC/2Ev/Ejercicios/T3/leerFichero/leerFichero.js
+++
@@ -1,4 +0,0 @@
-ficheroLeido = require('fs').readFileSync('/workspaces/portfolio_DAW/DWC/2Ev/Ejercicios/T3/leerFichero/mi_fichero.txt', 'utf8');
-var array = ficheroLeido.split("\n");
-console.log(array.sort().reverse());
-
